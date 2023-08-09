427b9b6414f92f06eccc70c207c5715121e84f3c
lukabakuradzee <130186938+lukabakuradzee@users.noreply.github.com>
2023-08-09 20:59:01 +0400

    updated js

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// BurgerMenu
+
 let mobileMenu = document.querySelector(".menu");
 let burgmenu = document.querySelector(".burgmenu");
 let xmark = document.querySelector(".xmark");
@@ -16,6 +18,9 @@ xmark.addEventListener("click", () => {
   });
 
 
+
+  // HEADER
+
   const header = document.querySelector('.header');
   let lastScrollPos = 0;
 
@@ -33,3 +38,77 @@ window.addEventListener('scroll', () => {
 
   lastScrollPos = currentScrollPos;
 });
+
+
+// Why Choose article 
+
+const whyChooseArticle = document.querySelector('.why_choose_article');
+
+const observerOptions = {
+  root: null,
+  rootMargin: '100px',
+  threshold: 0.5, // 50% of the element visible
+};
+
+const observer = new IntersectionObserver((entries, observer) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('visible');
+      observer.unobserve(entry.target); // Stop observing after making it visible
+    }
+  });
+}, observerOptions);
+
+observer.observe(whyChooseArticle);
+
+
+// section2 Why Choose Aritifice?
+
+document.addEventListener("DOMContentLoaded", () => {
+  const section2Heading = document.querySelector('.section2_heading');
+
+  const observerOptions = {
+    root: null,
+    rootMargin: '20px', // Positive rootMargin to start animation earlier
+    threshold: 0, // Trigger when even a pixel is visible
+  };
+
+  const observer = new IntersectionObserver((entries, observer) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('section2_heading_visible');
+        observer.unobserve(entry.target); // Stop observing after making it visible
+      }
+    });
+  }, observerOptions);
+
+  observer.observe(section2Heading);
+});
+
+
+// Transform Your Bussines With AI Technology (heading)
+
+
+document.addEventListener("DOMContentLoaded", () => {
+  const trustedByHeading = document.querySelector('.trusted_by_heading');
+
+  const observerOptions = {
+    root: null,
+    rootMargin: '50px', // Positive rootMargin to start animation earlier
+    threshold: 0.5, // Trigger when even a pixel is visible
+  };
+
+  const observer = new IntersectionObserver((entries, observer) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('heading_visible');
+        observer.unobserve(entry.target); // Stop observing after making it visible
+      }
+    });
+  }, observerOptions);
+
+  observer.observe(trustedByHeading);
+});
+
+
+
