d34e43e6efa9f90b44f6e4ce9ed76d5b4107e851
lukabakuradzee <lukabakuradzee@gmail.com>
2023-10-25 19:17:48 +0400

    added new elements, with js

--- script.js
+++ script.js
@@ -1,99 +1,106 @@
-'use strict';
+"use strict";
 
 // Continue website box
-document.addEventListener('DOMContentLoaded', () => {
-const buttonAlertBox = document.querySelector('.button-alert-box');
-const alertBox = document.querySelector('.alert-box');
+document.addEventListener("DOMContentLoaded", () => {
+  const buttonAlertBox = document.querySelector(".button-alert-box");
+  const alertBox = document.querySelector(".alert-box");
 
-buttonAlertBox.addEventListener('click', () => {
-  //  alertBox.remove();
-  alertBox.style.transform = 'translateX(200%)';
-  alertBox.style.transition = '2s ease';
-});
-
+  buttonAlertBox.addEventListener("click", () => {
+    //  alertBox.remove();
+    alertBox.style.transform = "translateX(200%)";
+    alertBox.style.transition = "2s ease";
+  });
 });
 
 // BurgerMenu
 
-document.addEventListener('DOMContentLoaded', () => {
-let mobileMenu = document.querySelector(".menu");
-let burgmenu = document.querySelector(".burgmenu");
-let xmark = document.querySelector(".xmark");
+document.addEventListener("DOMContentLoaded", () => {
+  let mobileMenu = document.querySelector(".menu");
+  let burgmenu = document.querySelector(".burgmenu");
+  let xmark = document.querySelector(".xmark");
 
-burgmenu.addEventListener("click", () => {
-mobileMenu.classList.add("show");
-burgmenu.style.display = "none";
-xmark.style.display = "block";
-
-});
-
-xmark.addEventListener("click", () => {
-  mobileMenu.classList.remove("show");
-  burgmenu.style.display = "block";
-  xmark.style.display = "none";
+  burgmenu.addEventListener("click", () => {
+    mobileMenu.classList.add("show");
+    burgmenu.style.display = "none";
+    xmark.style.display = "block";
   });
 
+  xmark.addEventListener("click", () => {
+    mobileMenu.classList.remove("show");
+    burgmenu.style.display = "block";
+    xmark.style.display = "none";
+  });
 });
 
+// Nav blog mouseover
 
+let blogMouserOver = document.querySelector(".blog");
+let blogBox = document.querySelector(".blog_box");
+let fadeTimeOut;
 
-  // HEADER
+blogMouserOver.addEventListener("mouseover", () => {
+  blogBox.style.display = "block";
+});
 
-  const header = document.querySelector('.header');
-  let lastScrollPos = 0;
-  
-  window.addEventListener('scroll', () => {
-    const currentScrollPos = window.pageYOffset;
-  
-    if (currentScrollPos > lastScrollPos) {
-      // Scrolling down, hide the header
-      header.style.top = '-100px';
-      header.style.backgroundColor = 'inherit';
-    } else {
-      // Scrolling up
-      if (currentScrollPos === 0) {
-        // At the top of the page, show the header with the inherited background color
-        header.style.backgroundColor = 'inherit';
-      } else if (currentScrollPos <= 100) {
-        // Scroll up slightly, transition the background color
-        const alpha = (currentScrollPos / 100) * 0.9; // Gradually increase alpha value from 0 to 0.9
+blogBox.addEventListener("mouseover", () => {
+  blogBox.style.display = "block";
+});
+
+blogBox.addEventListener("mouseout", () => {
+  blogBox.style.display = "none";
+});
+
+// HEADER
+
+const header = document.querySelector(".header");
+let lastScrollPos = 0;
+
+window.addEventListener("scroll", () => {
+  const currentScrollPos = window.pageYOffset;
+
+  if (currentScrollPos > lastScrollPos) {
+    // Scrolling down, hide the header
+    header.style.top = "-100px";
+    header.style.backgroundColor = "inherit";
+    blogBox.style.display = "none";
+  } else {
+    // Scrolling up
+    if (currentScrollPos === 0) {
+      // At the top of the page, show the header with the inherited background color
+      header.style.backgroundColor = "inherit";
+    } else if (currentScrollPos <= 100) {
+      // Scroll up slightly, transition the background color
+      const alpha = (currentScrollPos / 100) * 0.9; // Gradually increase alpha value from 0 to 0.9
       header.style.backgroundColor = `rgba(0, 12, 20, ${alpha})`;
-      header.style.transition = 'background-color 0.5s ease';
-
-      } else {
-        // Scrolling up further, maintain the specified background color
-        header.style.backgroundColor = 'rgba(0, 12, 20, 0.9)';
-        header.style.transition = 'background-color 0.5s ease';
-
-      }
-  
-      header.style.top = '0';
+      header.style.transition = "background-color 0.5s ease";
+    } else {
+      // Scrolling up further, maintain the specified background color
+      header.style.backgroundColor = "rgba(0, 12, 20, 0.9)";
+      header.style.transition = "background-color 0.5s ease";
     }
-  
-    lastScrollPos = currentScrollPos;
-  });
 
+    header.style.top = "0";
+  }
 
+  lastScrollPos = currentScrollPos;
+});
 
-  
-  
-  
-// Why Choose article 
+// Why Choose article
 
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener("DOMContentLoaded", () => {
   // Intersection Observer code
-  const whyChooseArticle = document.querySelector('.why_choose_article');
+  const whyChooseArticle = document.querySelector(".why_choose_article");
 
   const observerOptions = {
     root: null,
-    rootMargin: '100px',
+    rootMargin: "100px",
     threshold: 0.5,
   };
 
   const observer = new IntersectionObserver((entries, observer) => {
-    entries.forEach(entry => {
+    entries.forEach((entry) => {
       if (entry.isIntersecting) {
-        entry.target.classList.add('visible');
+        entry.target.classList.add("visible");
         observer.unobserve(entry.target);
       }
     });
@@ -102,24 +109,21 @@ document.addEventListener('DOMContentLoaded', () => {
   observer.observe(whyChooseArticle);
 });
 
-
-
-
 // section2 Why Choose Aritifice?
 
 document.addEventListener("DOMContentLoaded", () => {
-  const section2Heading = document.querySelector('.section2_heading');
+  const section2Heading = document.querySelector(".section2_heading");
 
   const observerOptions = {
     root: null,
-    rootMargin: '20px', // Positive rootMargin to start animation earlier
-    threshold: 0 // Trigger when even a pixel is visible
+    rootMargin: "20px", // Positive rootMargin to start animation earlier
+    threshold: 0, // Trigger when even a pixel is visible
   };
 
   const observer = new IntersectionObserver((entries, observer) => {
-    entries.forEach(entry => {
+    entries.forEach((entry) => {
       if (entry.isIntersecting) {
-        entry.target.classList.add('section2_heading_visible');
+        entry.target.classList.add("section2_heading_visible");
         observer.unobserve(entry.target); // Stop observing after making it visible
       }
     });
@@ -128,23 +132,21 @@ document.addEventListener("DOMContentLoaded", () => {
   observer.observe(section2Heading);
 });
 
-
 // Transform Your Bussines With AI Technology (heading)
 
-
 document.addEventListener("DOMContentLoaded", () => {
-  const trustedByHeading = document.querySelector('.trusted_by_heading');
+  const trustedByHeading = document.querySelector(".trusted_by_heading");
 
   const observerOptions = {
     root: null,
-    rootMargin: '50px',
-    threshold: 0.5, 
+    rootMargin: "50px",
+    threshold: 0.5,
   };
 
   const observer = new IntersectionObserver((entries, observer) => {
-    entries.forEach(entry => {
+    entries.forEach((entry) => {
       if (entry.isIntersecting) {
-        entry.target.classList.add('heading_visible');
+        entry.target.classList.add("heading_visible");
         observer.unobserve(entry.target); // Stop observing after making it visible
       }
     });
@@ -153,20 +155,19 @@ document.addEventListener("DOMContentLoaded", () => {
   observer.observe(trustedByHeading);
 });
 
-
 document.addEventListener("DOMContentLoaded", () => {
-  const section4Heading = document.querySelector('.section4_heading');
+  const section4Heading = document.querySelector(".section4_heading");
 
   const observerOptions = {
     root: null,
-    rootMargin: '20px', // Positive rootMargin to start animation earlier
+    rootMargin: "20px", // Positive rootMargin to start animation earlier
     threshold: 0, // Trigger when even a pixel is visible
   };
 
   const observer = new IntersectionObserver((entries, observer) => {
-    entries.forEach(entry => {
+    entries.forEach((entry) => {
       if (entry.isIntersecting) {
-        entry.target.classList.add('section4_heading_visible');
+        entry.target.classList.add("section4_heading_visible");
         observer.unobserve(entry.target); // Stop observing after making it visible
       }
     });
@@ -175,45 +176,42 @@ document.addEventListener("DOMContentLoaded", () => {
   observer.observe(section4Heading);
 });
 
+document.addEventListener("DOMContentLoaded", () => {
+  const buttonLoadMore = document.getElementById("button_load_more");
+  const cardAfterLoad = document.querySelector(".card_after_load");
 
-document.addEventListener('DOMContentLoaded', () => {
-  const buttonLoadMore = document.getElementById('button_load_more');
-  const cardAfterLoad = document.querySelector('.card_after_load');
-
-  buttonLoadMore.addEventListener('click', () => {
-    buttonLoadMore.textContent = 'Loading...';
+  buttonLoadMore.addEventListener("click", () => {
+    buttonLoadMore.textContent = "Loading...";
     // Disable the button temporarily
     buttonLoadMore.disabled = true;
 
     // Simulate a loading delay for 1 seconds
     setTimeout(() => {
-      buttonLoadMore.style.display = 'none';
-      cardAfterLoad.style.display = 'block';
-    }, 1000); 
+      buttonLoadMore.style.display = "none";
+      cardAfterLoad.style.display = "block";
+    }, 1000);
   });
 });
 
+document.addEventListener("DOMContentLoaded", () => {
+  const articles = document.querySelectorAll(".question_rows_div1");
 
-document.addEventListener('DOMContentLoaded', () => {
-  const articles = document.querySelectorAll('.question_rows_div1');
-
-  articles.forEach(article => {
+  articles.forEach((article) => {
     let contentVisible = false; // Initialize content visibility state
 
-    article.addEventListener('click', () => {
-      const hiddenParagraph = article.querySelector('.hidden-paragraph');
+    article.addEventListener("click", () => {
+      const hiddenParagraph = article.querySelector(".hidden-paragraph");
 
       if (contentVisible) {
-        hiddenParagraph.style.display = 'none'; 
+        hiddenParagraph.style.display = "none";
       } else {
-        hiddenParagraph.style.display = 'block'; 
+        hiddenParagraph.style.display = "block";
       }
 
       contentVisible = !contentVisible; // Toggle the content visibility state
     });
   });
 });
-
 
 // contact page clock
 function digitalClock() {
@@ -237,14 +235,5 @@ function digitalClock() {
   clock.textContent = formattedTime;
 }
 
-
 setInterval(digitalClock, 1000);
 digitalClock();
-
-
-
-
-
-
-
-
